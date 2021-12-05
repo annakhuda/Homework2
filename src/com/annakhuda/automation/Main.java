84336e10fc4de6d8bc904cbdca64c195b2547cd3
Anna Yadelska <anna_yadelska@epam.com>
2021-12-05 20:48:36 +0200

    Format sources

--- src/com/annakhuda/automation/Main.java
+++ src/com/annakhuda/automation/Main.java
@@ -7,15 +7,15 @@ import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResol
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The result of the first task is: " + (74+36));
-        System.out.println("\nSecond task, a variant: " + (-5+8*6));
-        System.out.println("Second task, b variant: " + ((55+9)%9));
-        System.out.println("Second task, c variant: " + (20+-3*5/8));
-        System.out.println("Second task d variant: " + (5+15/3*2-8%3));
+        System.out.println("The result of the first task is: " + (74 + 36));
+        System.out.println("\nSecond task, a variant: " + (-5 + 8 * 6));
+        System.out.println("Second task, b variant: " + ((55 + 9) % 9));
+        System.out.println("Second task, c variant: " + (20 + -3 * 5 / 8));
+        System.out.println("Second task d variant: " + (5 + 15 / 3 * 2 - 8 % 3));
         System.out.println("\n125 + 24 = " + sum(125, 24));
         System.out.println("125 - 24 = " + subtract(125, 24));
         System.out.println("125 x 24 = " + multiply(125, 24));
-        System.out.println("125 / 24 = " + divide(125, 24) );
+        System.out.println("125 / 24 = " + divide(125, 24));
         System.out.println("125 mod 24 = " + mod(125, 24));
         int[] arr = {5, 6, 7, 12, -5, 32, 43};
         int[] duplicatesArr = {5, 6, 7, 12, -5, 32, 43, 6, 12};
@@ -23,47 +23,47 @@ public class Main {
         System.out.println("\nDuplicates of the array: " + Arrays.toString(duplicates(duplicatesArr)));
     }
 
-    public static int sum (int firstNum, int secondNum){
+    public static int sum(int firstNum, int secondNum) {
         return firstNum + secondNum;
     }
 
-    public static int subtract(int firstNum, int secondNum){
+    public static int subtract(int firstNum, int secondNum) {
         return firstNum - secondNum;
     }
 
-    public static int multiply(int firstNum, int secondNum){
+    public static int multiply(int firstNum, int secondNum) {
         return firstNum * secondNum;
     }
 
-    public static int divide(int firstNum, int secondNum){
+    public static int divide(int firstNum, int secondNum) {
         return firstNum / secondNum;
     }
 
-    public static int mod(int firstNum, int secondNum){
+    public static int mod(int firstNum, int secondNum) {
         return firstNum % secondNum;
     }
 
-    public static int arraySum(int[] arr){
+    public static int arraySum(int[] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
         return sum;
     }
 
-    public static int[] duplicates (int[] arr){
+    public static int[] duplicates(int[] arr) {
         int[] duplFull = new int[10];
         int z = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++){
-                if(arr[j] == arr[i]){
-                    duplFull[z]=arr[j];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] == arr[i]) {
+                    duplFull[z] = arr[j];
                     z++;
                 }
             }
         }
-        int[] dupl = new int [z];
-        for (int i = 0; i < dupl.length; i++){
+        int[] dupl = new int[z];
+        for (int i = 0; i < dupl.length; i++) {
             dupl[i] = duplFull[i];
         }
         return dupl;
